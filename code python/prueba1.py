1f7e6619e1a33a0370cf9c6df5a92040bff36640
Juan Pablo García <100326982+JuanPabloGdlb@users.noreply.github.com>
2023-09-11 16:45:53 -0600

    Procesamiento por lotes 2.py

--- code python/prueba1.py
+++ code python/prueba1.py
@@ -1,46 +1,44 @@
-import os
-import shutil
-import random
-import string
+import os     # Importa la biblioteca os para operaciones de sistema de archivos
+import shutil # Importa la biblioteca shutil: es una biblioteca que proporciona operaciones en sistemas de archivos.
+import random # Importa la biblioteca random para generar números aleatorios
+import string # Importa la biblioteca string para acceder a cadenas de caracteres
 
-def copiar_y_modificar(carpeta_origen, carpeta_destino):
-    # Copiar la carpeta de origen a la carpeta de destino
-    shutil.copytree(carpeta_origen, carpeta_destino)
-
+def procesamientolotes(original, final):
+    # Copiar la carpeta de origen a la carpeta de destino, copytree se utiliza para copiar un directorio y todo su contenido a otro directorio.
+    shutil.copytree(original, final)
     # Función para generar una letra mayúscula aleatoria
     def letra_mayuscula_aleatoria():
         return random.choice(string.ascii_uppercase)
-
     # Función para generar un dígito aleatorio
     def digito_aleatorio():
         return random.choice(string.digits)
-
     # Recorrer todos los archivos en la carpeta de destino y sus subcarpetas
-    for ruta_actual, carpetas, archivos in os.walk(carpeta_destino):
+    for rActual, carpetas, archivos in os.walk(final):
         for archivo in archivos:
-            ruta_completa = os.path.join(ruta_actual, archivo)
+            ruta_completa = os.path.join(rActual, archivo)
+            #with: se usa para asegurarse de que el archivo se cierre automáticamente después de su uso
+            #r es para modo lectura
             with open(ruta_completa, 'r') as f:
-                contenido_original = f.read()
-            
+                contenido = f.read()
             # Modificar el contenido del archivo
-            contenido_modificado = ''
-            for caracter in contenido_original:
+            contenidocopiado = ''
+            for caracter in contenido:
+                #Estos if determinan que sí el caracter es letra, la cambia a digito y si es digito la cambia a letra
                 if caracter.isalpha():
                     # Cambiar letras por dígitos aleatorios
-                    contenido_modificado += digito_aleatorio()
+                    contenidocopiado += digito_aleatorio()
                 elif caracter.isdigit():
                     # Cambiar dígitos por letras mayúsculas aleatorias
-                    contenido_modificado += letra_mayuscula_aleatoria()
+                    contenidocopiado += letra_mayuscula_aleatoria()
+                #si es un caracter indefinido, se copia tal cual
                 else:
-                    contenido_modificado += caracter
-            
+                    contenidocopiado += caracter
             # Escribir el contenido modificado de vuelta al archivo
             with open(ruta_completa, 'w') as f:
-                f.write(contenido_modificado)
-
+                f.write(contenidocopiado)
+#Colocar las rutas                
 if __name__ == "__main__":
-    carpeta_origen = r'D:\tareas\tareas2023B\sem de so\prueba'
-    carpeta_destino = r'D:\tareas\tareas2023B\sem de so\copia_prueba'  # Cambia el nombre de la carpeta destino si es necesario
+    original = r'D:\tareas\tareas2023B\sem de so\prueba'
+    final = r'D:\tareas\tareas2023B\sem de so\copia_prueba'
 
-    copiar_y_modificar(carpeta_origen, carpeta_destino)
-    print(f"Copia de la carpeta modificada creada en {carpeta_destino}")
+    procesamientolotes(original, final)
